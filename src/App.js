adbb7ee11cbe407b09f296b5ee251309168bd3ef
Jeffrey <jjames.opspark@gmail.com>
2022-02-23 14:37:02 -0600

    passing tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-useless-constructor */
+import React from 'react'
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            index: index,
+        };
+        this.handleClick = this.handleClick.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    componentDidMount() {
+        this.updateIndexState = this.updateIndexState.bind(this);
+    }
+
+    handleClick() {
+        this.updateIndexState();
+    }
+
+    updateIndexState() {
+        this.setState({index: Math.floor(Math.random()*(Object.keys(quotes).length))});
+    }
+
+    render() {
+        return (
+                <div id="quote-box" style={{ border: '10px solid blue', justifyContent: 'center', textAlignVertical: 'center', textAlign: 'center'}}>
+                    <div id="text">
+                        Quote:&nbsp;&nbsp;
+                        {quotes[this.state.index].quote}
+                    </div>
+                    <div id="author">
+                        Author:&nbsp;&nbsp;
+                         {quotes[this.state.index].author}
+                    </div>
+                    <button onClick={this.handleClick} id="new-quote">
+                        Another Quote
+                    </button>
+                    <br /><a id="tweet-quote" href="https://twitter.com/intent/tweet">
+                        Twitter
+                    </a>
+                </div>
+                );
+    }
 }
 
-export default App;
+const quotes = [
+    {author: "I. N. Spired", quote: "Inspirational"},
+    {author: "A. N. Gry", quote: "I'm so mad."},
+    {author: "Lorraine Dofingbottom", quote: "Bottom's where it's at!"},
+    {author: "A. B. See", quote: "Dee Eee Fff"},
+    {author: "Pater Noster", quote: "Et tu Brute"}
+];
+
+const index = [Math.floor(Math.random()*(Object.keys(quotes).length))];
+
+console.log(index);
+
+export default App
